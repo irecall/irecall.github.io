f8b8912b3b4c8d84c2dd0012a2d123c25e59c99c
root <xxx@example.com>
2022-06-26 22:23:01 +0800

    update

--- maccms10-master/static/js/tj.js
+++ maccms10-master/static/js/tj.js
@@ -1,9 +1,1 @@
-document.write('<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f078d3cfd3b108dbce607f97adbb6137";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>')
+document.write('')
